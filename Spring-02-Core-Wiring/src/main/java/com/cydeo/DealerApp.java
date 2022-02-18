463f5bcad896d6b2563765f4c6dbb16d66d26a6d
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2022-02-17 20:21:29 -0500

    Wiring

---
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/DealerApp.java
@@ -0,0 +1,20 @@
+package com.cydeo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DealerApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigCar.class);
+
+        Car c = container.getBean(Car.class);
+        Person p = container.getBean(Person.class);
+
+        System.out.println("p.getName() = " + p.getName());
+        System.out.println("c.getMake() = " + c.getMake());
+        System.out.println("p.getCar().getMake() = " + p.getCar().getMake());
+
+
+    }
+}
